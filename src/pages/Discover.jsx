e631f0f3ede51b97999471c97e9ff129246cccdb
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-02-05 23:15:14 -0700

    adjusted Discorver page to the new api changes

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -9,19 +9,21 @@ const Discover = () => {
   const [genre, setGenre] = useState("POP");
   const { data, error, isLoading } = useGetSongByGenreQuery({ genre });
 
-  if (isLoading) return <Loader title="Loading songs..." />;
+  if (isLoading) return <Loader title='Loading songs...' />;
   if (error) return <Error />;
   function handleGenre(e) {
     setGenre(e.target.value);
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="w-full flex flex-col sm:flex-row justify-between items-center mt-4 mb-10   ">
-        <h2 className="font-bold text-3xl text-white text-left">{genres.find((g) => g.value === genre).title}</h2>
+    <div className='flex flex-col'>
+      <div className='w-full flex flex-col sm:flex-row justify-between items-center mt-4 mb-10'>
+        <h2 className='font-bold text-3xl text-white text-left'>
+          {genres.find((g) => g.value === genre).title}
+        </h2>
         <select
           onChange={handleGenre}
-          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
         >
           {genres.map((genre) => (
             <option key={genre.value} value={genre.value}>
@@ -31,17 +33,35 @@ const Discover = () => {
         </select>
       </div>
 
-      <div className="flex flex-wrap m:justify-start justify-center gap-8">
-        {data?.tracks.map((track, i) => (
+      <div className='flex flex-wrap m:justify-start justify-center gap-8'>
+        {data?.data?.map((track, i) => (
           <SongCard
             img={
-              track.images?.coverart ||
+              track.attributes?.artwork?.url?.replace("{w}x{h}", "500x500") ||
               "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.G0V_9qWp5vw22dxNqpNL7wHaHa%26pid%3DApi&f=1&ipt=748f3579ddf1724d1e87bcd12d33acfe465b24cdb51b81b66de9a3536ddbac94&ipo=images"
             }
-            title={track.title}
-            artist={track.subtitle}
-            key={track.key}
-            track={track}
+            title={track.attributes?.name}
+            artist={track.attributes?.artistName}
+            key={track.id}
+            track={{
+              ...track,
+              title: track.attributes?.name,
+              subtitle: track.attributes?.artistName,
+              images: {
+                coverart: track.attributes?.artwork?.url?.replace(
+                  "{w}x{h}",
+                  "500x500"
+                ),
+              },
+              hub: {
+                actions: [
+                  {},
+                  {
+                    uri: track.attributes?.previews?.[0]?.url,
+                  },
+                ],
+              },
+            }}
             isPlaying={isPlaying}
             activeSong={activeSong}
             data={data}
